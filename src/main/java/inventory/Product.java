57d3333314ef1bc816c9dcc544b67e32a5633e5d
Kim Charbonneau <kim.charbonneau@outlook.com>
2020-07-03 08:20:47 -0400

    Addind/Subtract products method done. Unavailable products not showing but still existing in db. Become available when added

--- src/main/java/inventory/Product.java
+++ src/main/java/inventory/Product.java
@@ -1,67 +1,74 @@
 package inventory;
 
-
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
+
 @Entity
 @Data
 public class Product {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
-	private String name;
-	private int quantity;
-	private String can;
-	private Type type;
-	
-	public Product() {}
 
-	public Long getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int quantity;
+    private String can;
+    private Type type;
+    private boolean available;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Product() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCan() {
-		return can;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public void setCan(String can) {
-		this.can = can;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	public Type getType() {
-		return type;
-	}
+    public String getCan() {
+        return can;
+    }
 
-	public void setType(Type type) {
-		this.type = type;
-	}
-	
-	
+    public void setCan(String can) {
+        this.can = can;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailability(boolean available) {
+        this.available = available;
+    }
 
 }
--- src/main/java/inventory/web/ProductController.java
+++ src/main/java/inventory/web/ProductController.java
@@ -2,6 +2,8 @@ package inventory.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,87 +14,103 @@ import inventory.Product;
 import inventory.Type;
 import inventory.service.ProductService;
 
-
 @Controller
 public class ProductController {
-	
-	@Autowired 
-	private ProductService productService;
-	
-	private List<Product> products;
-	
-	
-	@RequestMapping(method=RequestMethod.GET, value="/inventory")
-	public String showInventory(Model model) {
-		
-		products = productService.showInventory();
-		model.addAttribute("products", products);
-		return "inventory";
-		
-	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/add")
-	public String formForAddingProduct(){
-		return "add";
-	}
-	
-	@RequestMapping(method=RequestMethod.POST, value="/add")
-	public String addProduct(Product product, Type type) {
-		
-		boolean duplicate = false;
-		Long id;
-		int newQuantity=0;
-		
-		products = productService.showInventory();
-		
 
-		if(products.size() == 0)
-		{
-			productService.nameVerification(product.getName());
-			productService.standardizationCanned(product);
-			productService.addProduct(product);
-			
-		}else if(products.size() !=0)
-		{
-			
-			duplicate = productService.chekingForDuplicate(product.getName(), products);
-			
-			if(duplicate == true)
-			{
-				id = productService.findByName(product.getName());
-				Product existingProduct = productService.findById(id);
-				newQuantity = productService.addQuantity(product.getQuantity(),existingProduct.getQuantity());
-				productService.updateProductQuantity(newQuantity, id);
-			}else
-			{
-			productService.nameVerification(product.getName());
-			productService.standardizationCanned(product);
-			productService.addProduct(product);
-			}
-		}
-		return "add";
-		
-	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/delete")
-	public String formToDeleteProduct(){
-		return "delete";
-	}
-	
-	@RequestMapping(method=RequestMethod.POST, value="/delete")
-	public String deleteProduct(String name) {
-		
-		Long id = productService.findByName(name);
-		
-		if(id == null){
-			System.out.println("Couldn't find product to delete.");
-			
-		}
-		else
-		{
-			productService.deleteProductById(id);
-		}
-		return "delete";
-	}
+    @Autowired
+    private ProductService productService;
+
+    private List<Product> products;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/inventory")
+    public String showUIInventory(Model model) {
+
+        products = productService.uiInventory();
+        model.addAttribute("products", products);
+        return "inventory";
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{id}")
+    public String modifyInventory(Model model, HttpServletRequest request, Long id) {
+
+        Product selectedProduct = productService.findById(id);
+        int selectedProductQuantity = selectedProduct.getQuantity();
+        int result = 0;
+        int clicksNumber = 1;
+
+        String subtractButton = request.getParameter("Subtract");
+        String addButton = request.getParameter("Add");
+
+        if (subtractButton != null) {
+            result = productService.subtractQuantity(clicksNumber, selectedProductQuantity);
+        } else if (addButton != null) {
+            result = productService.addQuantity(clicksNumber, selectedProductQuantity);
+        }
+
+        productService.updateProductQuantity(result, id);
+        products = productService.uiInventory();
+        // Given name + array
+        model.addAttribute("products", products);
+        return "inventory";
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    public String formForAddingProduct() {
+        return "add";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/add")
+    public String addProduct(Product product, Type type) {
+
+        boolean duplicate = false;
+        Long id;
+        int newQuantity = 0;
+
+        products = productService.showInventory();
+
+        if (products.size() == 0) {
+            productService.nameVerification(product.getName());
+            productService.standardizationCanned(product);
+            productService.addProduct(product);
+
+        } else if (products.size() != 0) {
+
+            duplicate = productService.chekingForDuplicate(product.getName(), products);
+
+            if (duplicate == true) {
+                id = productService.findByName(product.getName());
+                Product existingProduct = productService.findById(id);
+                newQuantity = productService.addQuantity(product.getQuantity(), existingProduct.getQuantity());
+                productService.updateProductQuantity(newQuantity, id);
+            } else {
+                productService.nameVerification(product.getName());
+                productService.standardizationCanned(product);
+                productService.addProduct(product);
+            }
+        }
+        return "add";
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/delete")
+    public String formToDeleteProduct() {
+        return "delete";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/delete")
+    public String deleteProduct(String name) {
+
+        Long id = productService.findByName(name);
+
+        if (id == null) {
+            System.out.println("Couldn't find product to delete.");
+
+        } else {
+            productService.deleteProductById(id);
+        }
+        return "delete";
+    }
 
 }
